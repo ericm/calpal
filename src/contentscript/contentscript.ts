f7ef861976da8b4e0d7b69a12e9b5982c8e8d12e
Thomas Daniel Galligan <thomas007g@gmail.com>
2020-11-13 22:03:17 +0000

    added ui and started fetch utilities

--- src/contentscript/contentscript.ts
+++ src/contentscript/contentscript.ts
@@ -2,3 +2,25 @@ import './contentscript.scss';
 
 const isThisContentscript = true;
 console.log('isThisContentscript', isThisContentscript);
+
+const intialSetup = () => {
+
+  chrome.storage.local.set({
+    canvas: {
+      token: ""
+    }
+  })
+}
+
+interface ReqProps {
+  Method: string,
+  URL: string,
+}
+
+async function request(props: ReqProps): Promise<any> {
+  const resp = await fetch(props.URL, { method: props.Method });
+  const body = await resp.json();
+  return body;
+}
+
+
--- src/popup/src/App.tsx
+++ src/popup/src/App.tsx
@@ -1,25 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { Button, TextField, createStyles, makeStyles, Step, Stepper, StepLabel } from "@material-ui/core";
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#cf77d9',
+      light: '#cf77d9'
+    },
+    secondary: {
+      main: '#e6e6e6',
+      dark: '#2c2a47'
+    },
+  }
+})
+
+const steps = ["Provide Canvas API Token", "Authorize access to your Google Calendar"]
+
+const getStep = (stepIndex: number) => {
+  return steps[stepIndex]
+}
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    input: {
+      color: "white"
+    }
+  }),
+);
 
 function App() {
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const classes = useStyles();
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  }
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header>
+          <h1>CalPal</h1>
+          <small>Automating assignments, a few clicks away...</small>
+        </header>
+
+        <Stepper activeStep={activeStep} alternativeLabel>
+          {steps.map((label) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+
+        <div>
+          <TextField id="input-with-icon-grid" label="Canvas API Token" color="primary" InputProps={{ className: classes.input }} />
+          <Button onClick={handleNext} color="primary" variant="contained">Next</Button>
+          <Button onClick={handleBack} color="secondary" variant="contained">Back</Button>
+
+        </div>
+
+        <div className="oauthGrant">
+          <Button onClick={handleNext} color="primary" variant="contained">Authorize google calendar</Button>
+
+        </div>
+      </div >
+    </ThemeProvider>
   );
 }
 
